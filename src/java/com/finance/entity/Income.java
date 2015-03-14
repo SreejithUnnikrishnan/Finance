dcf2d1d3ae967409ece36c62cd3ee5b1e8369cf0
Sreejith Unnikrishnan <sreejithu393@gmail.com>
2015-03-14 16:55:45 -0400

    added Income.java with main user income budget

---
+++ src/java/com/finance/entity/Income.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.finance.entity;
+
+/**
+ *
+ * @author c0644881
+ */
+public class Income {
+    private int id;
+    private String name;
+    private String start_date;
+    private double budget;
+    private String user_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+    
+    
+}
