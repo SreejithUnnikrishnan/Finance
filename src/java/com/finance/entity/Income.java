e77d12d3053f55a461a4d2413725937712a87a72
SreejithUnnikrishnan <sreejithu393@gmail.com>
2015-03-29 12:38:31 -0400

    added income add for user

--- src/java/com/finance/entity/Income.java
+++ src/java/com/finance/entity/Income.java
@@ -3,18 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.finance.entity;
 
 import com.finance.database.DatabaseConnection;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ejb.Stateless;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.json.stream.JsonParser;
 
 /**
  *
@@ -22,6 +26,7 @@ import javax.json.JsonObjectBuilder;
  */
 @Stateless
 public class Income {
+
     private int id;
     private String name;
     private String start_date;
@@ -69,7 +74,7 @@ public class Income {
     }
 
     public String getIncomeDetails(String id) {
-        try(Connection connection = DatabaseConnection.getConnection()){
+        try (Connection connection = DatabaseConnection.getConnection()) {
             JsonArrayBuilder jarray = Json.createArrayBuilder();
             String query = "SELECT * FROM income WHERE user_id = ? and start_date >= DATE_FORMAT(NOW() ,'%Y-%m-01') AND start_date <= LAST_DAY(now())";
             PreparedStatement pstmt = connection.prepareStatement(query);
@@ -80,20 +85,104 @@ public class Income {
                         .add("id", rs.getInt("id"))
                         .add("name", rs.getString("name"))
                         .add("budget", rs.getDouble("budget"))
-                        .add("start_date", (rs.getDate("start_date")).toString())                                
+                        .add("start_date", (rs.getDate("start_date")).toString())
                         .add("user_id", rs.getInt("user_id"))
-                        .add("amount",rs.getDouble("amount"));
+                        .add("amount", rs.getDouble("amount"));
                 jarray.add(obj);
             }
-            
+
             return jarray.build().toString();
-            
-        }
-        catch(Exception ex){
-            System.out.println("Exception thrown in Get user Income Details: " +ex.getMessage());
+
+        } catch (Exception ex) {
+            System.out.println("Exception thrown in Get user Income Details: " + ex.getMessage());
             return null;
         }
     }
-    
-    
+
+    public String insertIncome(String details) {
+        JsonParser parser = Json.createParser(new StringReader(details));
+        Map<String, String> map = new HashMap<>();
+        String name = "", value;
+        while (parser.hasNext()) {
+            JsonParser.Event evt = parser.next();
+            switch (evt) {
+                case KEY_NAME:
+                    name = parser.getString();
+                    break;
+                case VALUE_STRING:
+                    value = parser.getString();
+                    map.put(name, value);
+                    break;
+                case VALUE_NUMBER:
+                    value = Integer.toString(parser.getInt());
+                    map.put(name, value);
+                    break;
+            }
+        }
+        int changes = 0;
+        String category = map.get("name");
+        String budget = map.get("budget");
+        String start_date = map.get("start_date");
+        String user_id = map.get("user_id");
+        String amount = map.get("amount");
+        int count = checkUserIncome(category, user_id);
+        if (count == -1) {
+            String query = "INSERT INTO income (name, budget, start_date, user_id, amount) VALUES (?, ?, now(), ?, ?)";
+            try (Connection connection = DatabaseConnection.getConnection()) {
+                PreparedStatement pstmt = connection.prepareStatement(query);
+                pstmt.setString(1, category);
+                pstmt.setString(2, budget);
+                pstmt.setString(3, user_id);
+                pstmt.setString(4, amount);
+                changes = pstmt.executeUpdate();
+                if (changes > 0) {
+                    return "success";
+                } else {
+                    return "fail";
+                }
+            } catch (SQLException ex) {
+                System.out.println("Sql Exception: " + ex.getMessage());
+                return "fail";
+            }
+        }
+        else{
+            String query = "update income set budget = ?, start_date = now(), amount = ? where id = ?";
+            try (Connection connection = DatabaseConnection.getConnection()) {
+                PreparedStatement pstmt = connection.prepareStatement(query);
+                pstmt.setString(1, budget);
+                pstmt.setString(2, amount);
+                pstmt.setInt(3, count);
+                changes = pstmt.executeUpdate();
+                if (changes > 0) {
+                    return "success";
+                } else {
+                    return "fail";
+                }
+            } catch (SQLException ex) {
+                System.out.println("Sql Exception: " + ex.getMessage());
+                return "fail";
+            }
+            
+        }
+    }
+
+    private int checkUserIncome(String category, String user_id) {
+        try (Connection connect = DatabaseConnection.getConnection()) {
+            String query = "select * from income where name = ? and user_id = ? and start_date >= DATE_FORMAT(NOW() ,'%Y-%m-01') AND start_date <= LAST_DAY(now())";
+            PreparedStatement pstmt = connect.prepareStatement(query);
+            pstmt.setString(1, category);
+            pstmt.setString(2, user_id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("id");
+            } else {
+                return -1;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception in user income check : " + ex.getMessage());
+            return -1;
+        }
+    }
+
 }
--- src/java/com/finance/services/IncomeService.java
+++ src/java/com/finance/services/IncomeService.java
@@ -7,9 +7,12 @@ package com.finance.services;
 
 import com.finance.entity.Income;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  *
@@ -23,6 +26,7 @@ public class IncomeService {
     
     @GET
     @Path("{id}")
+    @Produces("application/json")
     public String getUserIncome(@PathParam("id") String id){
         String result = income.getIncomeDetails(id);
         if(result.isEmpty() || result.equalsIgnoreCase("[]")){
@@ -35,5 +39,14 @@ public class IncomeService {
         }
         
     }
+    
+    @POST
+    @Consumes("application/json")
+    public String insertUserIncome(String details){
+        String result = income.insertIncome(details);
+        return result;
+        
+    }
+    
     
 }
